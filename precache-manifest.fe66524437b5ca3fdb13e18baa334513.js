ed86f63206c150abbf8f939de8967c22019a8508
naman-agg <naman.m4444@gmail.com>
2022-07-31 22:32:36 +0530

    Updates

---
+++ precache-manifest.fe66524437b5ca3fdb13e18baa334513.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2c3ca5ef2c31e1776a7edf93e0aca4",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "100159780ceddf97f075",
+    "url": "/home/static/css/main.e81f8812.chunk.css"
+  },
+  {
+    "revision": "0e34ee549e271f0d5f3b",
+    "url": "/home/static/js/2.924be112.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.924be112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100159780ceddf97f075",
+    "url": "/home/static/js/main.2845432a.chunk.js"
+  },
+  {
+    "revision": "83ac38a88332ee199197",
+    "url": "/home/static/js/runtime-main.5ee548bd.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
